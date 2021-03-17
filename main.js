567ed0fdabe3b2b2a3c92d45d42d71597188950a
France900 <francesco.em.livio@gmail.com>
2021-03-17 16:24:22 +0100

    a bit after the start
    sto usando molto aiuto esterno per compilare il js (troppo)

---
+++ main.js
@@ -0,0 +1,33 @@
+// Creare una Todo-list in cui si possano aggiungere nuovi todo e rimuoverli.
+// Rendiamo il tutto gradevole alla vista.
+// Bonus
+// Permettiamo la modifica di un todo già aggiunto.
+
+
+// $(document).ready(function () {
+//
+// })
+
+
+
+// input dati da aggiungere alla todolist
+var addListItem = $("#addListItem").val()
+
+
+// aggiungo input dati da aggiungere alla lista al click su botton
+
+// seleziono dove mi va il click
+$("#buttonPush")
+
+// la funzione del click
+
+
+
+
+
+
+
+
+
+// input dati da modificare
+// var ".cloneBay input" = $(".cloneBay input").val()
